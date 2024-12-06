8442768b529e245e296d29f26fb9d64a9bf0a030
Rama Diaz <ramadiaz221@gmail.com>
2024-12-06 14:04:36 +0700

    INIT of HeroVortex component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,8 @@
-import { HeroGrid } from "@/components/HeroGrid"
+'use client'
 
 const Page = () => {
   return (
     <div>
-      <div className="h-[calc(100vh-_150px)] w-2/5">
-        <HeroGrid />
-      </div>
     </div>
   )
 }
