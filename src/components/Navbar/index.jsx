1458d664e0d6383e104843af96018484bec82299
Rama Diaz <ramadiaz221@gmail.com>
2024-12-06 13:06:08 +0700

    UPDATE of Navbar components

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,24 @@
+'use client'
+
 import { Button, Image } from "@nextui-org/react"
+import { HandWaving, House, Info } from "@phosphor-icons/react"
+import { usePathname } from "next/navigation"
 
 const Navbar = () => {
+    const pathname = usePathname()
+
     return (
         <div className="flex flex-row justify-between items-center px-8 py-6">
-            <Image 
-                src="/logo/gm-light1.png" 
+            <Image
+                src="/logo/gm-light1.png"
                 isBlurred
                 width={170}
                 alt="GenggamMakna logo"
             />
-            <div>
-                Predict
+            <div className="flex flex-row items-center gap-6">
+                <House color="#333333" size={32} weight={pathname.endsWith("/") ? "fill" : "thin"} />
+                <HandWaving color="#333333" size={32} weight={pathname.endsWith("/predict") ? "fill" : "thin"} />
+                <Info color="#333333" size={32} weight={pathname.endsWith("/about") ? "fill" : "thin"} />
             </div>
             <div>
                 <Button className="bg-sky-600 text-white font-acorn" >Login</Button>
