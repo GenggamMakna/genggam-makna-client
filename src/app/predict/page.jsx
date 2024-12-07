45641ffbb112722bc533f03e876fde5e3eae6f29
Rama Diaz <ramadiaz221@gmail.com>
2024-12-07 17:11:23 +0700

    INIT of Tabs in /predict page

--- src/app/predict/page.jsx
+++ src/app/predict/page.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
 import { GetUserData } from "@/utilities/getUserData";
 import { toast } from "sonner";
+import { Tabs } from "@/components/ui/tabs";
 
 const Page = () => {
     const user_data = GetUserData()
@@ -21,6 +22,35 @@ const Page = () => {
         toast.error(message)
     }
 
+    const tabs = [
+        {
+            title: "Image",
+            value: "Image",
+            content: (
+                <div className="w-full overflow-hidden relative h-max rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-sky-300 to-blue-500">
+                    <p className="font-acorn">Image Predict</p>
+                    <div
+                        className="mt-8 w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-xl">
+                        <FileUpload onChange={handleFileUpload} onError={handleError} />
+                    </div>
+                </div>
+            ),
+        },
+        {
+            title: "Video",
+            value: "Video",
+            content: (
+                <div className="w-full overflow-hidden relative h-max rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-sky-300 to-blue-500">
+                    <p className="font-acorn">Video Predict</p>
+                    <div
+                        className="mt-8 w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-xl">
+                        <FileUpload onChange={handleFileUpload} onError={handleError} currentFormat="video" />
+                    </div>
+                </div>
+            ),
+        },
+    ];
+
     return (
         <div className="p-8 text-333">
             <div className="w-max mx-auto">
@@ -31,9 +61,8 @@ const Page = () => {
                     Upload an image or video of SIBI hand signs to translate it into text.
                 </h3>
             </div>
-            <div
-                className="mt-8 w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-xl">
-                <FileUpload onChange={handleFileUpload} onError={handleError} />
+            <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full mt-4 items-start justify-start">
+                <Tabs tabs={tabs} />
             </div>
             <div className="w-max mx-auto mt-4">
                 <HoverBorderGradient
