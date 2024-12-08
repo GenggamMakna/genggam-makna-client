507db15acc5ea59fdbe2c0c538b76a182227d1af
Rama Diaz <ramadiaz221@gmail.com>
2024-12-08 15:12:00 +0700

    INIT of /auth responsive

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -4,10 +4,10 @@ import { LogInContainer } from "@/components/LogInContainer"
 const Page = () => {
     return (
         <div className="w-full flex items-center justify-center">
-            <div className="max-w-xl w-full h-[calc(100vh-_170px)]">
+            <div className="max-w-xl w-full h-[calc(100vh-_170px)] hidden sm:block">
                 <HeroGrid />
             </div>
-            <div className="">
+            <div className="p-7 sm:p-0">
                 <LogInContainer />
             </div>
         </div>
